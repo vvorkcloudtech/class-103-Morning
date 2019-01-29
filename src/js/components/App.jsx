7d238526768c300d89dfd5cbc31089ab6ccd745f
Bilal Waheed <bwaheed598@gmai.com>
2019-01-29 13:56:58 +0500

    fkkffkfk

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,23 +1,12 @@
 import React from "react";
-<<<<<<< HEAD
-import Nav from './Nav';
-=======
 import Abdullah from 'Abdullah';
->>>>>>> 37a822714e91204ebde4478a30155fb40482b799
-
+import Bilal from './Bilal'
 class App extends React.Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <Nav/>
-=======
-        <h1>
-          Heading One Team Work 
-          </h1>
-
           <shujja />
->>>>>>> 37a822714e91204ebde4478a30155fb40482b799
+          <Bilal/>
       </div>
     );
   }

6d508d88b50d9448e4f9bde73a6db06235892523
softskills <nayabkirat@gmail.com>
2019-01-29 21:15:31 +0500

    nav componenet add

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Nav from "./Nav";
 
+// import { Route } from "react-router-dom";
 class App extends React.Component {
   render() {
     return (
       <div>
-     
+     <Nav />
       </div>
     );
   }
---
+++ src/js/components/Nav.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Data } from "./Data";
+
+
+class Nav extends React.Component {
+    state = {
+        data:Data
+    }
+  render() {
+      var stateData=this.state.data;
+      console.log(stateData)
+      var res = stateData.map((v,i)=>{
+        return <div key={i}>
+            <div className="nav">
+        <img src={v.Nav.img}/>
+        <ul className="list">
+            <li>{v.Nav.news} <i class="fas fa-angle-down"></i></li>
+            <li>{v.Nav.forums} <i class="fas fa-angle-down"></i></li>
+            <li>{v.Nav.DEVICES} <i class="fas fa-angle-down"></i></li>
+            <li>{v.Nav.topics} <i class="fas fa-angle-down"></i></li>
+            <li>{v.Nav.bestPosts} <i class="fas fa-angle-down"></i></li>
+        </ul>
+
+
+        
+        </div>
+        <div className="part-nav">
+        <ol className="tagbar-list">
+<li><span># </span>{v.Nav.tagbarlist.li1}</li>
+<li><span># </span>{v.Nav.tagbarlist.li2}</li>
+<li><span># </span>{v.Nav.tagbarlist.li3}</li>
+<li><span># </span>{v.Nav.tagbarlist.li4}</li>
+<li><span># </span>{v.Nav.tagbarlist.li5}</li>
+<li><span># </span>{v.Nav.tagbarlist.li6}</li>
+<li><span># </span>{v.Nav.tagbarlist.li7}</li>
+<li><span># </span>{v.Nav.tagbarlist.li8}</li>
+<li><span># </span>{v.Nav.tagbarlist.li9}</li>
+</ol>
+        </div>
+        </div>
+      })
+    return (
+      <div>
+          {res}
+        </div>
+    );
+  }
+}
+export default Nav;

b1a8feb8411e1a3c14f1738229f1fc3c5ce721e4
lionage <djfaisal300@gmail.com>
2019-01-29 13:16:45 +0500

    3rd update

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import Nav from './Nav';
 
+import Abdullah from 'Abdullah';
 
 class App extends React.Component {
   render() {
     return (
       <div>
+        
+        <Nav />
+
         <h1>
-          Heading One Team Work 
+          Heading One Team Work
           </h1>
-          <Testing />
+       
+          
+
+
+        <shujja />
+
       </div>
-      
     );
   }
 }

a8470a3907a1c493264d847a09881ad8d36bb262
softskills <nayabkirat@gmail.com>
2019-01-31 08:00:25 +0500

    nav

--- src/js/components/Nav.jsx
+++ src/js/components/Nav.jsx
@@ -11,6 +11,13 @@ class Nav extends React.Component {
         hover2:"",
         hover3:"",
         hover4:"",
+        hovers1:"",
+        hovers2:"",
+        hovers3:"",
+        hovers4:"",
+        hovers5:"",
+        hovers6:"",
+        hovers7:"",
     }
     mousefn = (e) =>{
         this.setState({
@@ -67,7 +74,86 @@ class Nav extends React.Component {
         })
     }
     
-    
+    //////start this devicse function
+    mousef1 = (e) =>{
+        this.setState({
+            hovers1:true,
+        
+        })
+    }
+    mousefs1 = (e) =>{
+        this.setState({
+            hovers1:false,
+            })
+    }
+    mousef2 = (e) =>{
+        this.setState({
+            hovers2:true,
+        
+        })
+    }
+    mousefs2 = (e) =>{
+        this.setState({
+            hovers2:false,
+            })
+    }
+    mousef3 = (e) =>{
+        this.setState({
+            hovers3:true,
+        
+        })
+    }
+    mousefs3 = (e) =>{
+        this.setState({
+            hovers3:false,
+            })
+    }
+    mousef4 = (e) =>{
+        this.setState({
+            hovers4:true,
+        
+        })
+    }
+    mousefs4 = (e) =>{
+        this.setState({
+            hovers4:false,
+            })
+    }
+    mousef5 = (e) =>{
+        this.setState({
+            hovers5:true,
+        
+        })
+    }
+    mousefs5 = (e) =>{
+        this.setState({
+            hovers5:false,
+            })
+    }
+    mousef6 = (e) =>{
+        this.setState({
+            hovers6:true,
+        
+        })
+    }
+    mousefs6 = (e) =>{
+        this.setState({
+            hovers6:false,
+            })
+    }
+    mousef7 = (e) =>{
+        this.setState({
+            hovers7:true,
+        
+        })
+    }
+    mousefs7 = (e) =>{
+        this.setState({
+            hovers7:false,
+            })
+    }
+
+
   render() {
       var stateData=this.state.data;
       console.log(stateData)
@@ -133,42 +219,161 @@ class Nav extends React.Component {
 
 
 
+
+
             <li onMouseEnter={this.mousefn2} onMouseLeave={this.mousefns2}>{v.Nav.DEVICES} <i className="fas fa-angle-down"></i>
             {this.state.hover2 ?
                <div className="nav-list2">
                <ul className="devices-list">
-                   <li>{v.Nav.deviceslist.LI1}</li>
-                   <li>{v.Nav.deviceslist.LI1}</li>
-                   <li>{v.Nav.deviceslist.LI1}</li>
-                   <li>{v.Nav.deviceslist.LI1}</li>
-                   <li>{v.Nav.deviceslist.LI1}</li>
+                   <li onMouseEnter={this.mousef1} onMouseLeave={this.mousefs1}>{v.Nav.deviceslist.LI1}
+                   {this.state.hovers1 ? 
+                   <div className="devices-list-start">
+            <ul className="deviceslist">
+            <li><span><img src={v.Nav.newsList.li1.img} /><p>{v.Nav.newsList.li1.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li2.img} /><p>{v.Nav.newsList.li2.p}</p></span></li> 
+            <li><span><img src={v.Nav.newsList.li4.img} /><p>{v.Nav.newsList.li4.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li10.img} /><p>{v.Nav.newsList.li10.p}</p></span></li>
+            </ul>
+            <div className="tip"></div>
+            </div>
+             : ""}
+                   </li>
+                   <li onMouseEnter={this.mousef2} onMouseLeave={this.mousefs2}>{v.Nav.deviceslist.LI1}
+                   {this.state.hovers2 ? 
+                   <div className="devices-list-start">
+            <ul className="deviceslist">
+            <li><span><img src={v.Nav.newsList.li7.img} /><p>{v.Nav.newsList.li7.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li8.img} /><p>{v.Nav.newsList.li8.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li9.img} /><p>{v.Nav.newsList.li9.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li10.img} /><p>{v.Nav.newsList.li10.p}</p></span></li>
+             </ul>
+            <div className="tip"></div>
+            </div>
+             : ""}
+                   </li>
+                   <li onMouseEnter={this.mousef3} onMouseLeave={this.mousefs3}>{v.Nav.deviceslist.LI1}
+                   {this.state.hovers3 ? 
+                   <div className="devices-list-start">
+            <ul className="deviceslist">
+            <li><span><img src={v.Nav.newsList.li9.img} /><p>{v.Nav.newsList.li9.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li1.img} /><p>{v.Nav.newsList.li1.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li2.img} /><p>{v.Nav.newsList.li2.p}</p></span></li> 
+           <li><span><img src={v.Nav.newsList.li10.img} /><p>{v.Nav.newsList.li10.p}</p></span></li>
+             </ul>
+            <div className="tip"></div>
+            </div>
+             : ""}
+             </li>
+             <li onMouseEnter={this.mousef4} onMouseLeave={this.mousefs4}>{v.Nav.deviceslist.LI1}
+                   {this.state.hovers4 ? 
+                   <div className="devices-list-start">
+            <ul className="deviceslist">
+            <li><span><img src={v.Nav.newsList.li7.img} /><p>{v.Nav.newsList.li7.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li8.img} /><p>{v.Nav.newsList.li8.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li10.img} /><p>{v.Nav.newsList.li10.p}</p></span></li>
+             </ul>
+            <div className="tip"></div>
+            </div>
+             : ""}
+             </li>
+             <li onMouseEnter={this.mousef5} onMouseLeave={this.mousefs5}>{v.Nav.deviceslist.LI1}
+                   {this.state.hovers5 ? 
+                   <div className="devices-list-start">
+            <ul className="deviceslist">
+            <li><span><img src={v.Nav.newsList.li7.img} /><p>{v.Nav.newsList.li7.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li8.img} /><p>{v.Nav.newsList.li8.p}</p></span></li>
+             </ul>
+            <div className="tip"></div>
+            </div>
+             : ""}
+             </li>
                    <li>{v.Nav.deviceslist.LI1}</li>
                    <li>{v.Nav.deviceslist.LI1}</li>
                    <li>{v.Nav.deviceslist.LI2}</li>
                    <li>{v.Nav.deviceslist.LI2}</li>
                </ul> <ul className="devices-list2">
-                   <li>{v.Nav.deviceslist.LI2}</li>
-                   <li>{v.Nav.deviceslist.LI2}</li>
+               <li onMouseEnter={this.mousef6} onMouseLeave={this.mousefs6}>{v.Nav.deviceslist.LI2}
+                   {this.state.hovers6 ? 
+                   <div className="devices-list-start">
+            <ul className="deviceslist">
+            <li><span><img src={v.Nav.newsList.li9.img} /><p>{v.Nav.newsList.li9.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li1.img} /><p>{v.Nav.newsList.li1.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li2.img} /><p>{v.Nav.newsList.li2.p}</p></span></li> 
+           <li><span><img src={v.Nav.newsList.li10.img} /><p>{v.Nav.newsList.li10.p}</p></span></li>
+             </ul>
+            <div className="tip"></div>
+            </div>
+             : ""}
+             </li> <li>{v.Nav.deviceslist.LI2}</li>
                    <li>{v.Nav.deviceslist.LI2}</li>
                    <li>{v.Nav.deviceslist.LI2}</li>
                    <li>{v.Nav.deviceslist.LI2}</li>
                </ul>
                 <ul className="devices-list3">
                    <li>{v.Nav.deviceslist.LI3}</li>
-                   <li>{v.Nav.deviceslist.LI2}</li>
-                   <li>{v.Nav.deviceslist.LI2}</li>
-                   <li>{v.Nav.deviceslist.LI2}</li>
+                   <li onMouseEnter={this.mousef7} onMouseLeave={this.mousefs7}>{v.Nav.deviceslist.LI3}
+                   {this.state.hovers7 ? 
+                   <div className="devices-list-start">
+            <ul className="deviceslist">
+            <li><span><img src={v.Nav.newsList.li9.img} /><p>{v.Nav.newsList.li9.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li1.img} /><p>{v.Nav.newsList.li1.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li2.img} /><p>{v.Nav.newsList.li2.p}</p></span></li> 
+             </ul>
+            <div className="tip"></div>
+            </div>
+             : ""}
+             </li> <li>{v.Nav.deviceslist.LI2}</li>
                    <li>{v.Nav.deviceslist.LI2}</li>
                    <li>{v.Nav.deviceslist.LI2}</li>
                </ul>
                </div>
                : ""}
             </li>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             <li onMouseEnter={this.mousefn3} onMouseLeave={this.mousefns3}>{v.Nav.topics} <i className="fas fa-angle-down"></i>
             {this.state.hover3 ?
                <div className="nav-list3">
                <ul className="topic-list">
-                   <li>{v.Nav.topiclist.LI1}</li>
+                   <li>{v.Nav.topiclist.LI1}
+                   {this.state.hover ? <div className="nav-list">
+            <ul className="news-list">
+            <li><span><img src={v.Nav.newsList.li1.img} /><p>{v.Nav.newsList.li1.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li2.img} /><p>{v.Nav.newsList.li2.p}</p></span></li> 
+            <li><span><img src={v.Nav.newsList.li3.img} /><p>{v.Nav.newsList.li3.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li4.img} /><p>{v.Nav.newsList.li4.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li5.img} /><p>{v.Nav.newsList.li5.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li6.img} /><p>{v.Nav.newsList.li6.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li7.img} /><p>{v.Nav.newsList.li7.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li8.img} /><p>{v.Nav.newsList.li8.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li9.img} /><p>{v.Nav.newsList.li9.p}</p></span></li>
+            <li><span><img src={v.Nav.newsList.li10.img} /><p>{v.Nav.newsList.li10.p}</p></span></li>
+            </ul>
+            </div>
+             : ""}
+       
+                   </li>
+
+
+
+
+
+
                    <li>{v.Nav.topiclist.LI1}</li>
                    <li>{v.Nav.topiclist.LI1}</li>
                    <li>{v.Nav.topiclist.LI1}</li>

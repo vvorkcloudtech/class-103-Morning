b3dc6d0868338e176baf27ad7916caee3cc2bd9c
softskills <nayabkirat@gmail.com>
2019-01-31 16:25:28 +0500

    nav file add in branch

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
-//Nav import start
 import Nav from "./Nav";
-import Forum from './Forums';
 import HonorHub from "./HonorHub";
 import AndroidPie from "./AndroidPie";
 import OnePlus6T from "./OnePlus6T";
@@ -11,30 +9,13 @@ import G5 from "./5G";
 import Pixel3 from "./Pixel3";
 import Samsung from "./Samsung";
 import Root from "./root";
-import PostCategory from "./PostCategory";
 import Mata20Pro from "./Mate20Pro";
-// Footer imports start
-import Footer from './Footer/Footer';
-import SecurityPolicy from './Footer/Secrity';
-import Contact from './Footer/Contact';
-import Advertise from './Footer/Advertise';
-import PrivacyPolicy from './Footer/PrivacyPolicy';
-import Rules from './Footer/Rules';
-import SuggestContent from './Footer/SuggestContent';
-import RootDevice from './Footer/RootDevice';
-// Footer imoorts ends
-import Tags from './tags';
-import Practice from './practice';
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        {/* <h1>
-          Heading One Team Work 
-          </h1>
-          <PostCategory/>
-          <Forum /> */}
      <Nav />
         <Route path="/HonorHub" component={HonorHub}/> 
         <Route path="/AndroidPie" component={AndroidPie}/>
@@ -45,21 +26,7 @@ class App extends React.Component {
         <Route path="/Samsung" component={Samsung}/>
         <Route path="/Root" component={Root}/>
         <Route path="/Mate20Pro" component={Mata20Pro}/>
-
-
-
-        <Route path="/tags" component={Tags}/>
         <Route path="/practice" component={Practice}/>
-{/* Footer starts here */}
-        <Footer/>
-        <Route path="/rules" component={Rules}/>
-        <Route path="/contact" component={Contact}/>
-        <Route path="/privacyPolicies" component={PrivacyPolicy}/>
-        <Route path="/rootDevice" component={RootDevice}/>
-        <Route path="/security" component={SecurityPolicy}/>
-        <Route path="/SuggestContent" component={SuggestContent}/>
-        <Route path="/advertise" component={Advertise}/>
-        {/* Footer ends Here */}
 
       </div>
     );
